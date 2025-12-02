a390066c8a1d75d682012f06a68b23d8d89bad9f
ANDY JOEL GUAMBA ROMAN <ajguambar@utn.edu.ec>
2025-12-02 12:48:19 -0500

    n11

---
+++ Examen/Migrations/20251202174744_v01.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Examen.Migrations
+{
+    /// <inheritdoc />
+    public partial class v01 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RegistroEstadistica",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TipoEstadistica = table.Column<int>(type: "int", nullable: false),
+                    Cantidad = table.Column<int>(type: "int", nullable: false),
+                    JugadorId = table.Column<int>(type: "int", nullable: false),
+                    PartidoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RegistroEstadistica", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Torneo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreTorneo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TipoTorneo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaInicio = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaFin = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Torneo", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Equipo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Grupo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TorneoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Equipo", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Equipo_Torneo_TorneoId",
+                        column: x => x.TorneoId,
+                        principalTable: "Torneo",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Partido",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Grupo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaPartido = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EquipoLocalId = table.Column<int>(type: "int", nullable: false),
+                    TorneoId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Partido", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Partido_Torneo_TorneoId",
+                        column: x => x.TorneoId,
+                        principalTable: "Torneo",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Jugador",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NumCamiseta = table.Column<int>(type: "int", nullable: false),
+                    EquipoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jugador", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Jugador_Equipo_EquipoId",
+                        column: x => x.EquipoId,
+                        principalTable: "Equipo",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Equipo_TorneoId",
+                table: "Equipo",
+                column: "TorneoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jugador_EquipoId",
+                table: "Jugador",
+                column: "EquipoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Partido_TorneoId",
+                table: "Partido",
+                column: "TorneoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Jugador");
+
+            migrationBuilder.DropTable(
+                name: "Partido");
+
+            migrationBuilder.DropTable(
+                name: "RegistroEstadistica");
+
+            migrationBuilder.DropTable(
+                name: "Equipo");
+
+            migrationBuilder.DropTable(
+                name: "Torneo");
+        }
+    }
+}
