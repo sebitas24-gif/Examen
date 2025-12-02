b19f5d86abf069b76e73968f8ae66d9cb170cfe2
ANDY JOEL GUAMBA ROMAN <ajguambar@utn.edu.ec>
2025-12-02 14:33:42 -0500

    n16

--- Examen/Data/ExamenContext.cs
+++ Examen/Data/ExamenContext.cs
@@ -5,20 +5,51 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using TorneoModelos;
 
-    public class ExamenContext : DbContext
+public class ExamenContext : DbContext
+{
+    public ExamenContext(DbContextOptions<ExamenContext> options)
+        : base(options)
     {
-        public ExamenContext (DbContextOptions<ExamenContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<TorneoModelos.Torneo> Torneos { get; set; } = default!;
-
-public DbSet<TorneoModelos.RegistroEstadistica> RegistroEstadisticas { get; set; } = default!;
-
-public DbSet<TorneoModelos.Partido> Partidos { get; set; } = default!;
-
-public DbSet<TorneoModelos.Jugador> Jugadors { get; set; } = default!;
-
-public DbSet<TorneoModelos.Equipo> Equipos { get; set; } = default!;
     }
+
+    // Tus tablas
+    public DbSet<Torneo> Torneos { get; set; } = default!;
+    public DbSet<RegistroEstadistica> RegistroEstadisticas { get; set; } = default!;
+    public DbSet<Partido> Partidos { get; set; } = default!;
+    public DbSet<Jugador> Jugadors { get; set; } = default!;
+    public DbSet<Equipo> Equipos { get; set; } = default!;
+
+    // CONFIGURACIÓN DE RELACIONES
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // 1. REGLAS PARA PARTIDOS (Ya las tenías)
+        // Evitar que al borrar un Equipo se borren los Partidos en cascada
+        modelBuilder.Entity<Partido>()
+            .HasOne(p => p.EquipoLocal)
+            .WithMany()
+            .HasForeignKey(p => p.EquipoLocalId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Partido>()
+            .HasOne(p => p.EquipoVisitante)
+            .WithMany()
+            .HasForeignKey(p => p.EquipoVisitanteId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        // 2. NUEVAS REGLAS PARA ESTADÍSTICAS (Esto arregla tu error actual)
+        // Evitar que al borrar un Partido o Jugador se borren las estadísticas en cascada
+        modelBuilder.Entity<RegistroEstadistica>()
+            .HasOne(re => re.Partido)
+            .WithMany(p => p.Estadisticas)
+            .HasForeignKey(re => re.PartidoId)
+            .OnDelete(DeleteBehavior.Restrict); // <--- AQUÍ ESTÁ LA SOLUCIÓN
+
+        modelBuilder.Entity<RegistroEstadistica>()
+            .HasOne(re => re.Jugador)
+            .WithMany()
+            .HasForeignKey(re => re.JugadorId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
---
+++ Examen/Migrations/20251202193253_v01.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Examen.Migrations
+{
+    /// <inheritdoc />
+    public partial class v01 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Torneos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tipo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaInicio = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaFin = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Torneos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Equipos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Grupo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TorneoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Equipos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Equipos_Torneos_TorneoId",
+                        column: x => x.TorneoId,
+                        principalTable: "Torneos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Jugadors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreCompleto = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NumCamiseta = table.Column<int>(type: "int", nullable: false),
+                    EquipoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jugadors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Jugadors_Equipos_EquipoId",
+                        column: x => x.EquipoId,
+                        principalTable: "Equipos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Partidos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Grupo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaPartido = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TorneoId = table.Column<int>(type: "int", nullable: false),
+                    EquipoLocalId = table.Column<int>(type: "int", nullable: false),
+                    EquipoVisitanteId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Partidos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Partidos_Equipos_EquipoLocalId",
+                        column: x => x.EquipoLocalId,
+                        principalTable: "Equipos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Partidos_Equipos_EquipoVisitanteId",
+                        column: x => x.EquipoVisitanteId,
+                        principalTable: "Equipos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Partidos_Torneos_TorneoId",
+                        column: x => x.TorneoId,
+                        principalTable: "Torneos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RegistroEstadisticas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TipoEstadistica = table.Column<int>(type: "int", nullable: false),
+                    Cantidad = table.Column<int>(type: "int", nullable: false),
+                    JugadorId = table.Column<int>(type: "int", nullable: false),
+                    PartidoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RegistroEstadisticas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RegistroEstadisticas_Jugadors_JugadorId",
+                        column: x => x.JugadorId,
+                        principalTable: "Jugadors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_RegistroEstadisticas_Partidos_PartidoId",
+                        column: x => x.PartidoId,
+                        principalTable: "Partidos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Equipos_TorneoId",
+                table: "Equipos",
+                column: "TorneoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jugadors_EquipoId",
+                table: "Jugadors",
+                column: "EquipoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Partidos_EquipoLocalId",
+                table: "Partidos",
+                column: "EquipoLocalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Partidos_EquipoVisitanteId",
+                table: "Partidos",
+                column: "EquipoVisitanteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Partidos_TorneoId",
+                table: "Partidos",
+                column: "TorneoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RegistroEstadisticas_JugadorId",
+                table: "RegistroEstadisticas",
+                column: "JugadorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RegistroEstadisticas_PartidoId",
+                table: "RegistroEstadisticas",
+                column: "PartidoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RegistroEstadisticas");
+
+            migrationBuilder.DropTable(
+                name: "Jugadors");
+
+            migrationBuilder.DropTable(
+                name: "Partidos");
+
+            migrationBuilder.DropTable(
+                name: "Equipos");
+
+            migrationBuilder.DropTable(
+                name: "Torneos");
+        }
+    }
+}
