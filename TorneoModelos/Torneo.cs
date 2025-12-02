381617ab0aebbd593b60aefd155049a0d8d071e6
ANDY JOEL GUAMBA ROMAN <ajguambar@utn.edu.ec>
2025-12-02 12:09:46 -0500

    new2

--- TorneoModelos/Torneo.cs
+++ TorneoModelos/Torneo.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace TorneoModelos
 {
     class Torneo
     {
+        [Key] public int Id { get; set; }
+        public string NombreTorneo { get; set; }
+        public string TipoTorneo { get; set; } // "Liga", "Copa", "Mixto" 
+
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public List<Equipo>? Equipos { get; set; }
+        public List<Partido>? Partidos { get; set; }
     }
 }
