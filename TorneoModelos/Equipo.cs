c089cf2d1e8a193001a035c142ab10ee742bc8c8
ANDY JOEL GUAMBA ROMAN <ajguambar@utn.edu.ec>
2025-12-02 12:15:10 -0500

    new4

---
+++ TorneoModelos/Equipo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorneoModelos
+{
+    internal class Equipo
+    {
+        
+        [Key]public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string? Grupo { get; set; }
+        public int TorneoId { get; set; }
+        [ForeignKey("TorneoId")]  public Torneo Torneo { get; set; }
+        public List<Jugador> Jugadores { get; set; }
+    }
+}
